82f75e8a779317492c315604936162d4160518f3
aoly <alyyasser19@gmail.com>
2020-04-16 19:15:01 +0200

    test

---
+++ src/exceptions/noHeroSelectedException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class noHeroSelectedException extends HearthstoneException {
+    public noHeroSelectedException()
+    {
+        super();
+    }
+    public noHeroSelectedException(String message)
+    {
+        super(message);
+    }
+}
--- src/view/mainMenu.java
+++ src/view/mainMenu.java
@@ -1,5 +1,6 @@
 package view;
 
+import exceptions.CannotAttackException;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -7,13 +8,14 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import model.heroes.Hero;
-import model.heroes.Mage;
+import model.heroes.*;
 
 import javax.sound.sampled.*;
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 
 public class mainMenu extends Application {
 
@@ -66,20 +68,28 @@ public class mainMenu extends Application {
         priest.setPrefSize(100,100);
         characters.setHgap(30);
         characters.setVgap(30);
+
+        //Farida add an Image to every Hero
         characters.add(mage,17,7);
         characters.add(hunter,17,8);
         characters.add(priest,18,7);
         characters.add(paladin,18,8);
         characters.add(warlock,19,7);
         characters.add(select,19,8);
+        HBox left= new HBox();
+        HBox right=new HBox();
         wholeScreen.setCenter(characters);
+        wholeScreen.setLeft(left);
+        wholeScreen.setRight(right);
         characterSelect= new Scene(wholeScreen);
         start.setOnMouseClicked(e->{
             titleScreen.setScene(characterSelect);
         });
 
         //Hero Selection
+        selected=false;
         mage.setOnMouseClicked(e->{
+            if(!selected){
             try {
                 selectedHero= new Mage();
             } catch (IOException ioException) {
@@ -87,9 +97,120 @@ public class mainMenu extends Application {
             } catch (CloneNotSupportedException cloneNotSupportedException) {
                 cloneNotSupportedException.printStackTrace();
             }
-            wholeScreen.setLeft();
+            //Farida: insert Character image to the left.
+            //selectedCharacter= new ImageView(new Image("images/main menu.jpg"));
+            //left.getChildren().add(selectedCharacter);
+                                 }
         });
+        mage.setOnMouseClicked(e->{
+                try {
+                    selectedHero= new Mage();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                } catch (CloneNotSupportedException cloneNotSupportedException) {
+                    cloneNotSupportedException.printStackTrace();
+                }
+            if(!selected){
+                //Farida: insert Character image to the left.
+                //selectedCharacter= new ImageView(new Image("images/main menu.jpg"));
+                //left.getChildren().add(selectedCharacter);
+            }
+            if(selected){
+                //Farida: insert Character image to the right.
+                //selectedCharacter= new ImageView(new Image("images/main menu.jpg"));
+                //left.getChildren().add(selectedCharacter);
+            }
+        });
+        warlock.setOnMouseClicked(e->{
+            try {
+                selectedHero= new Warlock();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            } catch (CloneNotSupportedException cloneNotSupportedException) {
+                cloneNotSupportedException.printStackTrace();
+            }
+            if(!selected){
+                //Farida: insert Character image to the left.
+                //selectedCharacter= new ImageView(new Image("images/main menu.jpg"));
+                //left.getChildren().add(selectedCharacter);
+            }
+            if(selected){
+                //Farida: insert Character image to the right.
+                //selectedCharacter= new ImageView(new Image("images/main menu.jpg"));
+                //left.getChildren().add(selectedCharacter);
+            }
+        });
+        paladin.setOnMouseClicked(e->{
+            try {
+                selectedHero= new Paladin();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            } catch (CloneNotSupportedException cloneNotSupportedException) {
+                cloneNotSupportedException.printStackTrace();
+            }
+            if(!selected){
+                //Farida: insert Character image to the left.
+                //selectedCharacter= new ImageView(new Image("images/main menu.jpg"));
+                //left.getChildren().add(selectedCharacter);
+            }
+            if(selected){
+                //Farida: insert Character image to the right.
+                //selectedCharacter= new ImageView(new Image("images/main menu.jpg"));
+                //left.getChildren().add(selectedCharacter);
+            }
+        });
+        priest.setOnMouseClicked(e->{
+            try {
+                selectedHero= new Priest();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            } catch (CloneNotSupportedException cloneNotSupportedException) {
+                cloneNotSupportedException.printStackTrace();
+            }
+            if(!selected){
+                //Farida: insert Character image to the left.
+                //selectedCharacter= new ImageView(new Image("images/main menu.jpg"));
+                //left.getChildren().add(selectedCharacter);
+            }
+            if(selected){
+                //Farida: insert Character image to the right.
+                //selectedCharacter= new ImageView(new Image("images/main menu.jpg"));
+                //left.getChildren().add(selectedCharacter);
+            }
+        });
+        hunter.setOnMouseClicked(e->{
+            try {
+                selectedHero= new Hunter();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            } catch (CloneNotSupportedException cloneNotSupportedException) {
+                cloneNotSupportedException.printStackTrace();
+            }
+            if(!selected){
+                //Farida: insert Character image to the left.
+                //selectedCharacter= new ImageView(new Image("images/main menu.jpg"));
+                //left.getChildren().add(selectedCharacter);
+            }
+            if(selected){
+                //Farida: insert Character image to the right.
+                //selectedCharacter= new ImageView(new Image("images/main menu.jpg"));
+                //left.getChildren().add(selectedCharacter);
+            }
+        });
+        select.setOnMouseClicked(e->{
+            if(selectedHero!=null){
+            if(!selected){
+            player1= selectedHero;
+            selected=true;
+            selectedHero=null;}
+            if(selected)
+                player2=selectedHero;
+        }
+            else
+                throw new noHeroSelectedException("This minion Cannot Attack");
 
+        }
+        );
 
 
 
